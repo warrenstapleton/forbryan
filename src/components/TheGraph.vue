e18de9a5b0b577b9b8a9d8d7f5c9cb6e77b96bd0
Bryan Stapleton <stapleton_bryan@yahoo.com>
2023-10-11 00:33:28 -0600

    fixed flex issue + consolidated css; tsconfig needs fix

--- src/components/TheGraph.vue
+++ src/components/TheGraph.vue
@@ -1,57 +1,72 @@
 <script setup lang="ts">
-
-import {ref, onMounted, onBeforeMount} from "vue";
+import { ref, onMounted, onBeforeMount } from 'vue'
 // import './style.css';
-import {type CellStyle, Client, Graph, InternalEvent, RubberBandHandler} from '@maxgraph/core';
-import {registerCustomShapes} from "./custom-shapes";
-import {useGraphStore} from "@/stores/graph.ts";
-
+import { type CellStyle, Client, Graph, InternalEvent, RubberBandHandler } from '@maxgraph/core'
+import { registerCustomShapes } from './custom-shapes'
+import { useGraphStore } from '@/stores/graph.ts'
 
 // display the maxGraph version in the footer
 // const footer = document.querySelector<HTMLElement>('footer')!;
 // footer.innerText = `Built with maxGraph ${Client.VERSION}`;
 
 onMounted(() => {
-  const {graph, initializeGraph} = useGraphStore()
+  const { graph, initializeGraph } = useGraphStore()
 
   // Create the graph inside the given container
-  initializeGraph(<HTMLElement>document.getElementById('graph-container'));
+  initializeGraph(<HTMLElement>document.getElementById('graph-container'))
 })
 
 onMounted(() => {
-  const {graph, initializeGraph} = useGraphStore()
+  const { graph, initializeGraph } = useGraphStore()
 
   console.log(graph)
   // shapes and styles
-  registerCustomShapes();
+  registerCustomShapes()
   // @ts-ignore TODO fix TS2532: Object is possibly 'undefined'.
-  graph.getStylesheet().getDefaultEdgeStyle().edgeStyle = 'orthogonalEdgeStyle'; // TODO use constants.EDGESTYLE instead of 'orthogonalEdgeStyle'
+  graph.getStylesheet().getDefaultEdgeStyle().edgeStyle = 'orthogonalEdgeStyle' // TODO use constants.EDGESTYLE instead of 'orthogonalEdgeStyle'
 
   // Gets the default parent for inserting new cells. This
   // is normally the first child of the root (ie. layer 0).
-  const parent = graph.getDefaultParent();
+  const parent = graph.getDefaultParent()
 
   // Adds cells to the model in a single step
   graph.batchUpdate(() => {
-    const vertex01 = graph.insertVertex(parent, null, 'a regular rectangle', 10, 10, 100, 100);
-    const vertex02 = graph.insertVertex(parent, null, 'a regular ellipse', 350, 90, 50, 50, <CellStyle>{
+    const vertex01 = graph.insertVertex(parent, null, 'a regular rectangle', 10, 10, 100, 100)
+    const vertex02 = graph.insertVertex(parent, null, 'a regular ellipse', 350, 90, 50, 50, <
+      CellStyle
+    >{
       shape: 'ellipse',
       fillColor: 'orange'
-    });
-    graph.insertEdge(parent, null, 'a regular edge', vertex01, vertex02);
+    })
+    graph.insertEdge(parent, null, 'a regular edge', vertex01, vertex02)
 
     // insert vertices using custom shapes
     // TODO type issue in CellStyle type, shape should allow string to manage custom implementation
-    const vertex11 = graph.insertVertex(parent, null, 'a custom rectangle', 20, 200, 100, 100, /*<CellStyle>*/{shape: 'customRectangle'});
-    const vertex12 = graph.insertVertex(parent, null, 'a custom ellipse', 150, 350, 70, 70, /*<CellStyle>*/{shape: 'customEllipse'});
-    graph.insertEdge(parent, null, 'another edge', vertex11, vertex12);
-  });
+    const vertex11 = graph.insertVertex(
+      parent,
+      null,
+      'a custom rectangle',
+      20,
+      200,
+      100,
+      100,
+      /*<CellStyle>*/ { shape: 'customRectangle' }
+    )
+    const vertex12 = graph.insertVertex(
+      parent,
+      null,
+      'a custom ellipse',
+      150,
+      350,
+      70,
+      70,
+      /*<CellStyle>*/ { shape: 'customEllipse' }
+    )
+    graph.insertEdge(parent, null, 'another edge', vertex11, vertex12)
+  })
 })
-
 </script>
 
 <template>
-  <div id="graph-container">
-    Hello Warren
-  </div>
+  <div id="graph-container"></div>
 </template>
--- src/views/GraphView.vue
+++ src/views/GraphView.vue
@@ -1,60 +1,80 @@
 <script setup lang="ts">
+//@ts-ignore
 import TheGraph from '../components/TheGraph.vue'
 </script>
 
 <template>
-    <div class="editor-container">
-      <div class="editor-toolbar">Toolbar</div>
-      <div class="editor-main">
-        <div class="editor-library">
-          <p>
-            Library 2
-          </p>
-        </div>
-        <div class="editor-graph">
-          Hi Warren
-<!--          <TheGraph/>-->
-        </div>
-        <div class="editor-properties">
-          <p>
-            Properties
-          </p>
-        </div>
+  <div class="editor-container">
+    <div class="editor-toolbar">
+      Toolbar <button class="tb-btn">test</button><button class="tb-btn">test2</button>
+    </div>
+    <div class="editor-main">
+      <div class="editor-library">
+        <p>Library 2</p>
+      </div>
+      <div class="editor-graph">
+        <TheGraph />
+      </div>
+      <div class="editor-properties">
+        <p>Properties</p>
       </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
+/* remove width + margin in .editor-container to set back to full size */
 .editor-container {
+  display: flex;
   flex-direction: column;
-  width: 100vw;
-  height: 100vh;
-}
-.editor-toolbar {
+  width: 90%;
   margin: auto;
-  background-color: red;
+  height: 80vh;
 }
+.editor-container > * {
+  display: flex;
+}
+
+/* place-content can be set to center or flex-end to move toolbar items to the center or right, respectively */
+.editor-toolbar {
+  background-color: red;
+  place-items: center;
+  place-content: start;
+  padding: 0.35em;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.editor-toolbar > * {
+  margin: 0px 2px;
+}
+
 .editor-main {
-  flex-direction: row;
+  height: 100%;
 }
 .editor-graph {
-  width: 50%;
+  width: 75%;
 }
 .editor-library {
   flex-direction: column;
-  width: 20%;
-  background-color: blue;
+  width: 12.5%;
+  background: linear-gradient(36deg, blue, lightblue);
 }
 .editor-properties {
   flex-direction: column;
-  width: 20%;
-  background-color: purple;
+  width: 12.5%;
+  background: linear-gradient(65deg, lightblue, purple);
 }
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+
+.tb-btn {
+  background-color: white;
+  border: none;
+  padding: 0.35em;
+  border-radius: 0.35em;
+  transition: background-color 0.4s;
+}
+
+.tb-btn:hover {
+  background-color: pink;
 }
 </style>
